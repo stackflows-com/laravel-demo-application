1a076dc4066e68cc2fe6565956bc127b94eb21e3
Sergey A <sergey.v.akkuzin@gmail.com>
2021-06-30 12:49:46 +0300

    SG-51: create a basic layout with navigation

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,16 @@
+<header class="Header">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Throw signal</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">User tasks</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
