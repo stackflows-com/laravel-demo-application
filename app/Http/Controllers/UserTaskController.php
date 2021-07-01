99b2377127926f730ebcab971e4c6091f4609b27
Sergey A <sergey.v.akkuzin@gmail.com>
2021-07-01 18:29:40 +0300

    SG-45: create a user task completion functionality

--- app/Http/Controllers/UserTaskController.php
+++ app/Http/Controllers/UserTaskController.php
@@ -23,4 +23,39 @@ class UserTaskController extends Controller
 
         return view('user_tasks.index', compact('tasks'));
     }
+
+    /**
+     * Dispatch the task completion.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function complete($id)
+    {
+        $task = UserTask::findOrFail($id);
+
+        CompleteTaskJob::dispatch($task);
+
+        $task->status->set(TaskStatus::SENDING);
+        $task->save();
+
+        return response()->json([], 204);
+    }
+
+    /**
+     * Dispatch the completion of all tasks.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function completeAll()
+    {
+        $tasks = UserTask::where('status', TaskStatus::PENDING)->get();
+        foreach ($tasks as $task) {
+            CompleteTaskJob::dispatch($task);
+        }
+
+        UserTask::whereIn('id', $tasks->pluck('id'))->update(['status' => TaskStatus::SENDING]);
+
+        return redirect()->back();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,5 @@ Route::get('/throw-signal', [SignalController::class, 'create'])->name('signals.
 Route::post('/throw-signal', [SignalController::class, 'store'])->name('signals.store');
 
 Route::get('/user-tasks', [UserTaskController::class, 'index'])->name('user_tasks.index');
+Route::post('/user-tasks/{id}/complete', [UserTaskController::class, 'complete'])->name('user_tasks.complete');
+Route::post('/user-tasks/complete-all', [UserTaskController::class, 'completeAll'])->name('user_tasks.complete_all');
