9ff5716d9be85c9124bb56cce5799a2d32af4443
Sergey A <sergey.v.akkuzin@gmail.com>
2021-07-13 14:07:09 +0300

    Add a middleware to verify auth on the backoffice

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::get('/throw-signal', [SignalController::class, 'create'])->name('signals.create');
-Route::post('/throw-signal', [SignalController::class, 'store'])->name('signals.store');
+Route::middleware(['backofficeAuth'])->group(function () {
+    Route::get('/throw-signal', [SignalController::class, 'create'])->name('signals.create');
+    Route::post('/throw-signal', [SignalController::class, 'store'])->name('signals.store');
 
-Route::get('/user-tasks', [UserTaskController::class, 'index'])->name('user_tasks.index');
-Route::post('/user-tasks/{id}/complete', [UserTaskController::class, 'complete'])->name('user_tasks.complete');
-Route::post('/user-tasks/complete-all', [UserTaskController::class, 'completeAll'])->name('user_tasks.complete_all');
+    Route::get('/user-tasks', [UserTaskController::class, 'index'])->name('user_tasks.index');
+    Route::post('/user-tasks/{id}/complete', [UserTaskController::class, 'complete'])->name('user_tasks.complete');
+    Route::post('/user-tasks/complete-all', [UserTaskController::class, 'completeAll'])
+        ->name('user_tasks.complete_all');
+});
