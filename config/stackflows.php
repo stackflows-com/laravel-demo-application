86e1ab5895ed949ae4246e2180ca30d61fcce540
Simonas Šerlinskas <saimaz@users.noreply.github.com>
2021-10-25 20:11:09 +0300

    more model support

--- config/stackflows.php
+++ config/stackflows.php
@@ -1,14 +1,16 @@
 <?php
 
 return [
-    // Address of the Stack Flow Gateway API.
-    'gatewayHost' => env('STACKFLOWS_GATEWAY_HOST'),
+    // Address of the Stack Flows API.
+    'apiHost' => env('STACKFLOWS_API_HOST'),
 
-    // Gateway Auth token
+    // API Auth token
     'authToken' => env('STACKFLOWS_AUTH_TOKEN'),
+    'environmentToken' => env('STACKFLOWS_ENVIRONMENT_TOKEN'),
 
     'external_task_executors' => [
-        \App\Bpmn\Tasks\RetrieveVehicleActiveCargoesTaskExecutor::class
+        \App\Bpmn\Tasks\RetrieveVehicleActiveCargoesTaskExecutor::class,
+        \App\Bpmn\Tasks\RetrieveActiveLastTaskTaskExecutor::class,
     ],
 
     'user_task_sync' => [],
